e413a083b7eb54a9346c0a8f7cc773e5841c7111
Felipe <fagundesdeveloper@gmail.com>
2022-06-08 10:59:09 -0300

    birds

---
+++ _next/static/nM4vS2k6IV8IfBSdnt00N/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/99-e94c79ce8e8c91b8.js","static/chunks/pages/index-354704a114e011ac.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
