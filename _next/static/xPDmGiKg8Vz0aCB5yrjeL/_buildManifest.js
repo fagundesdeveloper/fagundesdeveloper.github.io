97585e34843639b191a1b9841b8001fb9452534e
Felipe <fagundesdeveloper@gmail.com>
2022-12-01 18:24:27 -0300

    d

--- _next/static/xPDmGiKg8Vz0aCB5yrjeL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/99-e94c79ce8e8c91b8.js","static/chunks/pages/index-31aa48b6073fc15e.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
